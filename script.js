d0e647d22f1a34bf70346e1548a847fd0448b4e4
Swapnilpatil03 <swapnilpatil20040@gmail.com>
2024-12-09 20:48:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
