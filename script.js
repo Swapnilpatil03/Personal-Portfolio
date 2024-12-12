12a945f8887e2f730dd0ed7bf830be494bf976b6
Swapnilpatil03 <swapnilpatil20040@gmail.com>
2024-12-12 22:28:47 +0530

    Project section has been added

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function openModal(modalId) {
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
+
+function navigateSmoothly(url) {
+    // Add a fade-out class to the body
+    document.body.classList.add('fade-out');
+    // Wait for the animation to complete before navigating
+    setTimeout(() => {
+        location.href = url;
+    }, 500); // Match the transition duration in CSS
+}
+function moveSlider(direction) {
+    const container = document.querySelector('.projects-container');
+    const cardWidth = document.querySelector('.project-card').offsetWidth;
+    const currentScroll = container.scrollLeft;
+
+    if (direction === 'next') {
+        container.scrollLeft = currentScroll + cardWidth;
+    } else {
+        container.scrollLeft = currentScroll - cardWidth;
+    }
+     container.scrollTo({
+left: targetScroll,
+behavior: 'smooth'
+});
+}
